29c78898406ef0f849621e92aab6b1b98f77bf95
Ohm <ohmmsons@gmail.com>
2022-12-26 16:42:43 +0000

    Added Models

--- server/src/models/message.ts
+++ server/src/models/message.ts
@@ -1,10 +1,17 @@
 import mongoose from "mongoose"
 const msgSchema = new mongoose.Schema({
-    msg:{
-        type:String,
-        required:true
-    }
-})
+        conversationId: {
+            type: String,
+        },
+        sender: {
+            type: String,
+        },
+        text: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+)
 
 const Msg = mongoose.model('msg',msgSchema)
 module.exports = Msg;
